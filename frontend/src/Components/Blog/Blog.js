3aff2e860de9ddba7e0f38d4749d0e0df48179a4
Jay-Sojitra <84234572+Jay-Sojitra@users.noreply.github.com>
2022-09-25 18:10:55 +0530

    blog and blogPage added

---
+++ frontend/src/Components/Blog/Blog.js
@@ -0,0 +1,136 @@
+import React from 'react'
+import Header from '../Common/Header'
+import Tag from '../Common/Tag'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
+const Blogs = () => {
+
+    const visibilityStyle = {
+        backgroundColor: "#D5E3FE",
+        color: "#2C5EFF",
+        fontSize: 15,
+        fontWeight: "bold",
+        margin: "4px"
+    };
+
+    const rightButton = {
+        backgroundColor: "#D5F0E1",
+        color: "#52B67D",
+        fontSize: 15,
+        fontWeight: "bold",
+
+        width: 'max-content',
+        marginLeft: '3px'
+    };
+
+    const ToprightButton = {
+        backgroundColor: "#D5F0E1",
+        color: "#52B67D",
+        fontSize: 13,
+        fontWeight: "bold",
+
+    };
+
+    const data = [
+        {
+            title: 'Blockchain',
+            description: 'Machine Learning is the field of study that gives computers the capability to learn without being explicitly programmed. ML is one of the most exciting technologies that one would have ever come across. As it is evident from the name, it gives the computer that makes it more similar to humans: The ability to learn. Machine learning is actively being used today, perhaps in many more places than one would expect.',
+            tags: [
+                'JavaScript', 'React', 'Next'
+            ],
+            Author: 'Jay Sojitra',
+            branch: 'IT',
+            Like: '1.4'
+        },
+        {
+            title: 'Best Resourses need to top in  MBBS First Year',
+            description: 'Machine Learning is the field of study that gives computers the capability to learn without being explicitly programmed. ML is one of the most exciting technologies that one would have ever come across. As it is evident from the name, it gives the computer that makes it more similar to humans: The ability to learn. Machine learning is actively being used today, perhaps in many more places than one would expect.',
+            tags: [
+                'JavaScript', 'React', 'Next'
+            ],
+            Author: 'Siddharth Patel',
+            branch: 'IT',
+            Like: '1.6'
+        },
+        {
+            title: 'Machine Learning',
+            description: 'Machine Learning is the field of study that gives computers the capability to learn without being explicitly programmed. ML is one of the most exciting technologies that one would have ever come across. As it is evident from the name, it gives the computer that makes it more similar to humans: The ability to learn. Machine learning is actively being used today, perhaps in many more places than one would expect.',
+            tags: [
+                'JavaScript', 'React', 'Next'
+            ],
+            Author: 'Dharmesh Vala',
+            branch: 'CE',
+            Like: '1.4'
+        }
+    ]
+    return (
+        <>
+            <Header index="3" />
+            <div className="d-flex justify-content-center flex-column mt-3">
+                {
+                    data.map((blogs, i) => {
+                        return (
+                            <div class="card mb-3 d-flex justify-content-center" style={{
+                                width: '80%',
+                                margin: 'auto',
+                                borderRadius: '7px',
+                                boxShadow: 'rgb(0 0 0 / 25%) 5px 5px 7px'
+                            }}>
+                                <div class="card-body">
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <div>
+                                            <h5 class="card-title text-start ">{blogs.title}</h5>
+                                        </div>
+                                        <div className='d-flex justify-content-evenly align-items-center' style={{ width: '11%' }}>
+                                            <Tag
+                                                title={blogs.branch}
+                                                customeStyle={ToprightButton}
+                                            />
+                                            <p class="card-text text-end"><small class="text-muted">{blogs.Like} K</small> <ThumbUpIcon style={{marginBottom: "6px"}} /></p>
+
+                                        </div>
+                                    </div>
+
+                                    <p class="card-text text-start">{blogs.description}</p>
+                                    <div className='d-flex justify-content-between' style={{ marginTop: '12px' }}>
+
+                                        <div className='d-flex justify-content-evenly' style={{
+                                            width: '30%',
+                                        }}>
+                                            {blogs.tags.map((tag, i) => {
+                                                return (
+                                                    <Tag
+                                                        title={tag}
+                                                        customeStyle={visibilityStyle}
+                                                    />)
+                                            })}
+
+                                        </div>
+                                        <div className="d-flex justify-content-evenly align-items-center">
+
+                                            <div><b>Author</b> :
+
+                                            </div>
+                                            <div>
+                                                <Tag
+                                                    title={blogs.Author}
+                                                    customeStyle={rightButton}
+                                                />
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+
+
+            </div>
+        </>
+    )
+}
+
+export default Blogs
--- frontend/src/Components/Common/Header.js
+++ frontend/src/Components/Common/Header.js
@@ -9,59 +9,59 @@ export default function Header(props) {
     <>
       <div>
         <div className="main-container-header">
-        <div style={{ }}>
-        <NavLink className="nav-link" to="/project">
-          <div className="">
-            <img
-              src="img/nv1.jpg"
-              className="homeimg"
-              style={{ width: 70, height: 70, marginTop: "14px" }}
-             
-            />
+          <div style={{}}>
+            <NavLink className="nav-link" to="/project">
+              <div className="">
+                <img
+                  src="img/nv1.jpg"
+                  className="homeimg"
+                  style={{ width: 70, height: 70, marginTop: "14px" }}
+
+                />
+              </div>
+            </NavLink>
           </div>
-        </NavLink>
-      </div>
           <div>
             <div className="tab-container-header">
               <div className="tab-container">
                 <NavLink className=
-                {(index==1)? "nav-link active" : "nav-link" } to="/mentor">
+                  {(index == 1) ? "nav-link active" : "nav-link"} to="/">
                   Find Mentor
                 </NavLink>
               </div>
-             
-              
+
+
               <div className="tab-container">
-                <NavLink className={(index==3)? "nav-link active" : "nav-link" } to="/blog">
+                <NavLink className={(index == 3) ? "nav-link active" : "nav-link"} to="/blog">
                   Blog
                 </NavLink>
               </div>
               <div className="tab-container">
-                <NavLink className={(index==5)? "nav-link active" : "nav-link" } to="/roadmap">
-                 Roadmap
+                <NavLink className={(index == 5) ? "nav-link active" : "nav-link"} to="/roadmap">
+                  Roadmap
                 </NavLink>
               </div>
               <div className="tab-container">
-                <NavLink className={(index==6)? "nav-link active" : "nav-link" } to="/community">
+                <NavLink className={(index == 6) ? "nav-link active" : "nav-link"} to="/community">
                   Community
                 </NavLink>
               </div>
               <div className="tab-container">
-                <NavLink className={(index==6)? "nav-link active" : "nav-link" } to="/resources">
+                <NavLink className={(index == 6) ? "nav-link active" : "nav-link"} to="/resources">
                   Resources
                 </NavLink>
               </div>
               <div className="tab-container">
-                <NavLink className={(index==0)? "nav-link active" : "nav-link" } to="/aboutus">
+                <NavLink className={(index == 0) ? "nav-link active" : "nav-link"} to="/aboutus">
                   About
                 </NavLink>
               </div>
               <div className="tab-container">
-                <NavLink className={(index==4)? "nav-link active" : "nav-link" } to="/contact">
+                <NavLink className={(index == 4) ? "nav-link active" : "nav-link"} to="/contact">
                   Contact
                 </NavLink>
               </div>
-             
+
             </div>
           </div>
 
@@ -81,8 +81,9 @@ export default function Header(props) {
                     src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
 
                     className="w-110 h-110 rounded-circle"
-                    style={{ width: 70, height: 70,marginTop: "14px" }}
+                    style={{ width: 70, height: 70, marginTop: "14px" }}
                     height="100px"
+                    alt=''
                   />
                 </div>
               </NavLink>
