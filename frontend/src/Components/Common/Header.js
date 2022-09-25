01a4b1df62c53dd6c4b66d5604a52431452d9077
Dharmesh Vala <dvala453@gmail.com>
2022-09-25 22:50:25 +0530

    f

--- frontend/src/Components/Common/Header.js
+++ frontend/src/Components/Common/Header.js
@@ -25,7 +25,7 @@ export default function Header(props) {
             <div className="tab-container-header">
               <div className="tab-container">
                 <NavLink className=
-                {(index==1)? "nav-link active" : "nav-link" } to="/mentor">
+                {(index==1)? "nav-link active black" : "nav-link" } to="/mentor">
                   Find Mentor
                 </NavLink>
               </div>
