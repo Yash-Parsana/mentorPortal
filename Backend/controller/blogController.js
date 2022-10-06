cdbb396f7605b5dbbb81a88dfd9bc08846c218a1
Jay-Sojitra <84234572+Jay-Sojitra@users.noreply.github.com>
2022-10-07 02:53:19 +0530

    modified blog and event apis

--- Backend/controller/blogController.js
+++ Backend/controller/blogController.js
@@ -1,24 +1,55 @@
 const blog = require('../models/blogMode')
 
-const getAllBlogs = async(req, res, next)=> {
-    
+
+const addBlog = async (req, res, next) => {
+
+    res.set('Access-Control-Allow-Origin', '*');
+
+    try {
+        // const { name, mentorId, speaker, time, duration, description, image } = req.body;
+        const { title, mentor, date, tag, like,type, feedback } = req.body;
+        let newblog = new blog();
+        newblog.title = title;
+        newblog.mentor = mentor;
+        newblog.date = date;
+        newblog.tag = tag;
+        newblog.like = like;
+        newblog.type = type;
+        newblog.feedback = feedback;
+        await newblog.save();
+        res.status(200).json({
+            success: true,
+            message: "Event is saved", newblog
+        })
+    }
+    catch (err) {
+        console.log("Error while saving new event : ", err);
+        res.status(400).json({
+            success: false,
+            message: "Event can not be saved"
+        })
+    }
+
+
+}
+
+const getAllBlogs = async (req, res, next) => {
+
     let myblogs;
     try {
         myblogs = blog.find();
     }
-    catch (err)
-    {
-        console.log("Error while fetching all blogs",err);
+    catch (err) {
+        console.log("Error while fetching all blogs", err);
         res.status(400).json({
             success: false,
-            message:"Blogs are not avaiable right now"
+            message: "Blogs are not avaiable right now"
         })
     }
-    if (!myblogs)
-    {
+    if (!myblogs) {
         res.status(404).json({
             success: false,
-            message:"Blogs are not found"
+            message: "Blogs are not found"
         })
     }
     res.status(200).json({
@@ -27,26 +58,24 @@ const getAllBlogs = async(req, res, next)=> {
     })
 }
 
-const getBlogsByAuthorId = async (req,res,next) => {
-    
+const getBlogsByAuthorId = async (req, res, next) => {
+
     let id = req.params
     let myblogs;
     try {
-        myblogs=blog.find({authorId:id})
+        myblogs = blog.find({ _id: id }).populate('mentor')
     }
-    catch (err)
-    {
+    catch (err) {
         console.log("Error while fetching blogs by author id: ", err);
         res.status(400).json({
             success: false,
-            message:"Blogs are not found"
+            message: "Blogs are not found"
         })
     }
-    if (!myblogs)
-    {
+    if (!myblogs) {
         res.status(404).json({
             success: false,
-            message:"Blogs are not found"
+            message: "Blogs are not found"
         })
     }
     res.status(200).json({
@@ -55,4 +84,4 @@ const getBlogsByAuthorId = async (req,res,next) => {
     })
 }
 
-module.exports = { getAllBlogs, getBlogsByAuthorId };
+module.exports = { getAllBlogs, getBlogsByAuthorId ,addBlog};
