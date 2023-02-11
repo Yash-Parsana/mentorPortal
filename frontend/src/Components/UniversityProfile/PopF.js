65219dc816aec81b8b28f6932a2c5a64267301b3
Dharmesh Vala <dvala453@gmail.com>
2023-02-11 08:06:52 +0530

    done

--- frontend/src/Components/UniversityProfile/PopF.js
+++ frontend/src/Components/UniversityProfile/PopF.js
@@ -1,265 +1,148 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import Button from "../Common/Button";
 import Modal from "./PopUpFrom/Modal";
 import "./PopUpFrom/styles.css";
-import ProfileInputField from "../Profile/ProfileInputField";
+// import ProfileInputField from "../Profile/ProfileInputField";
 import axios from "../../helpers/axios";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import "./pop.css";
-import Cookies from "universal-cookie";
-import { Navigate } from "react-router-dom";
+// import Cookies from "universal-cookie";
+// import { Navigate } from "react-router-dom";
 
-class Pop extends React.Component {
-  constructor(props) {
-    super(props);
+function PopF() {
+  const [name, setName] = useState("");
+  const [duration, setDuration] = useState("");
+  const [description, setDescription] = useState("");
+  const [img, setImg] = useState("");
+  const [modal, setModal] = useState(false);
+  const speaker = "507f1f77bcf86cd799439011";
+  const mentor = "507f1f77bcf86cd799439011";
 
-    this.state = {
-      modal: false,
-      title: "",
-      tagline: "",
-      imglink: "",
-      desc:"",
-      speaker:"",
-      date:"",
-      duration:"",
-      redirect: false,
-    };
-  }
-
-  componentDidMount() {
-    // const sendReq = async () => {
-    //     const res = await axios
-    //       .get(`http://localhost:5000/api/department/getByCollgeId/${CollegeId}`)
-    //       .catch((err) => console.log(err));
-    //     const data = await res.data;
-    //     //console.log("Data from API:" + data[0].Fname);
-    //     return data;
-    //   };
-    // sendReq().then((data) => {
-    //     this.setState({ dept: data.departments });
-    //   console.log(this.state.dept);
-    // });
-  }
-
-  handleChange(e) {
-    const target = e.target;
-    const name = target.name;
-    const value = target.value;
-
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit(e) {
-    this.setState({ name: this.state.modalInputName });
-    this.modalClose();
-  }
-
-  modalOpen() {
-    this.setState({ modal: true });
-  }
-
-  modalClose() {
-    this.setState({
-      title: "",
-      modal: false,
-    });
-  }
-
-  render() {
-    const sendRequest = async () => {
-      const res = await axios
-        .post("/events/addevent", {
-       
-          name: this.state.title,
-          mentor:"507f1f77bcf86cd799439011",
-          speaker:"507f1f77bcf86cd799439011",
-          duration:this.state.duration,
-          description:this.state.desc,
-          image:this.state.imglink
-        })
-        .catch((err) => console.log('err',err));
-      const data = await res.data;
-      return data;
-    };
-    function refreshPage() {
-      window.location.reload(false);
+  const sendRequest = async (e) => {
+    e.preventDefault();
+    console.log("hhhhhhhhhhhhhhhhhhhhhhh")
+    try {
+      const res = await axios.post("/events/addevent", {
+        name,
+        mentor,
+        speaker,
+        duration,
+        description,
+        image:img,
+      });
+      console.log("res.data", res.data);
+    } catch (err) {
+      console.log("err", err);
     }
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(this.state.title);
+    modalClose();
+  };
 
-      sendRequest()
-        .then((data) => console.log(data))
-        .then(() => {
-          this.state.redirect && <navigate to="/event" replace={true} />;
-        });
-      this.modalClose();
-      // refreshPage();
-    };
-    const { dept } = this.state;
-    return (
-      <div className="App">
-        <a href="javascript:;" onClick={(e) => this.modalOpen(e)}>
-          <div
-            style={{
-              marginTop: 20,
-              justifyContent: "end",
+  const handleSubmit = (e) => {
+    modalClose();
+  };
 
-              display: "flex",
-              marginBottom: "50px",
-              marginRight: "20px",
-            }}
-          >
-            <Button title={"+ Add Event"} />
-          </div>
-        </a>
-        <Modal show={this.state.modal} handleClose={(e) => this.modalClose(e)}>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <div className="facultypop">
-                Event Name <span style={{ color: "red" }}>*</span>
-              </div>
-              <div style={{ width: "-webkit-fill-available" }}>
-                <input
-                  className="form-control input-field"
-                  name="title"
-                  title={"Event title"}
-                  onChange={(e) => this.handleChange(e)}
-                  placeholder={"Ex: Awareness about blood donation"}
-                  value={this.state.title}
-                />
-              </div>
-              <div className="facultypop">
-                Tagline <span style={{ color: "red" }}>*</span>
-              </div>
-              <div style={{ width: "-webkit-fill-available" }}>
-                <input
-                  className="form-control input-field"
-                  name="tagline"
-                  title={"Tagline:"}
-                  onChange={(e) => this.handleChange(e)}
-                  placeholder={"Ex: once a bvmites a always bvmites"}
-                  value={this.state.tagline}
-                />
-              </div>
+  const modalOpen = () => {
+    // setModal((current) => !current);
+    setModal(true);
+  };
 
-              <div className="facultypop">
-                Duration <span style={{ color: "red" }}>*</span>
-              </div>
+  const modalClose = () => {
+    // setModal((current) => !current);
+    setModal(false);
+  };
+  return (
+    <div className="App">
+      <a href="javascript:;" onClick={modalOpen}>
+        <div
+          style={{
+            marginTop: 20,
+            justifyContent: "end",
+            display: "flex",
+            marginBottom: "50px",
+            marginRight: "20px",
+          }}
+        >
+          <Button title={"+ Add Event"} />
+        </div>
+      </a>
+      <Modal show={modal} handleClose={modalClose}>
+        <form onSubmit={sendRequest} >
+          <div className="form-group">
+            <div className="facultypop">
+              Event Name <span style={{ color: "red" }}>*</span>
+            </div>
+            <div style={{ width: "-webkit-fill-available" }}>
+              <input
+                className="form-control input-field"
+                title={"Event title"}
+                onChange={(e) => setName(e.target.value)}
+                placeholder={"Ex: Awareness about blood donation"}
+                value={name}
+              />
+            </div>
+            <div className="facultypop">
+              Tagline <span style={{ color: "red" }}>*</span>
+            </div>
 
-              <div style={{ width: "-webkit-fill-available" }}>
-                <input
-                  className="form-control input-field"
-                  name="duration"
-                  title={"Duration of Event"}
-                  onChange={(e) => this.handleChange(e)}
-                  placeholder={"Ex: 45min"}
-                  value={this.state.duration}
-                />
-              </div>
-
-              <div className="facultypop">
-                Date <span style={{ color: "red" }}>*</span>
-              </div>
-
-              <div style={{ width: "-webkit-fill-available" }}>
-                <input
-                  className="form-control input-field"
-                  name="date"
-                  title={"Date of Event"}
-                  onChange={(e) => this.handleChange(e)}
-                  placeholder={"Ex: 23rd November, 2023"}
-                  value={this.state.date}
-                />
-              </div>
-
-              <div className="facultypop">
-              Speaker Name <span style={{ color: "red" }}>*</span>
+            <div className="facultypop">
+              Duration <span style={{ color: "red" }}>*</span>
             </div>
 
             <div style={{ width: "-webkit-fill-available" }}>
               <input
                 className="form-control input-field"
-                name="speaker"
-                title={"speaker of Event"}
-                onChange={(e) => this.handleChange(e)}
-                placeholder={"Ex: Dharmesh Vala"}
-                value={this.state.speaker}
+                title={"Duration of Event"}
+                onChange={(e) => setDuration(e.target.value)}
+                placeholder={"Ex: 45min"}
+                value={duration}
               />
             </div>
-            <div className="facultypop">
-            Mentor Name <span style={{ color: "red" }}>*</span>
-          </div>
-
-          <div style={{ width: "-webkit-fill-available" }}>
-            <input
-              className="form-control input-field"
-              name="mentor"
-              title={"mentor of Event"}
-              onChange={(e) => this.handleChange(e)}
-              placeholder={"Ex: Dharmesh Vala"}
-              value={this.state.mentor}
-            />
-          </div>
 
             <div className="facultypop">
-            Description <span style={{ color: "red" }}>*</span>
-          </div>
-
-          <div style={{ width: "-webkit-fill-available" }}>
-            <input
-              className="form-control input-field"
-              name="desc"
-              title={"Description of Event"}
-              onChange={(e) => this.handleChange(e)}
-              placeholder={"Ex: Enter Description"}
-              value={this.state.desc}
-            />
-          </div>
-
-              <div className="facultypop">Poster Link</div>
-              <div style={{ width: "-webkit-fill-available" }}>
-                <input
-                  className="form-control input-field"
-                  name="imglink"
-                  title={"Event Photo Link"}
-                  onChange={(e) => this.handleChange(e)}
-                  placeholder={"Ex: Upload link like drive link"}
-                  value={this.state.imglink}
-                />
-              </div>
+              Description <span style={{ color: "red" }}>*</span>
             </div>
-            <div className="form-group">
-              <div
-                style={{
-                  marginTop: 20,
-                  justifyContent: "center",
-                  display: "flex",
-                  marginRight: "20px",
-                }}
-              >
-                <Button
-                  title={"Submit"}
-                  // onClick={(e) => this.handleSubmit(e)}
-                />
-              </div>
-            </div>
-          </form>
-        </Modal>
-      </div>
-    );
-  }
-}
 
-function PopF() {
-  return (
-    <div>
-      <Pop />
+            <div style={{ width: "-webkit-fill-available" }}>
+              <input
+                className="form-control input-field"
+                title={"Description of Event"}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder={"Ex: Enter Description"}
+                value={description}
+              />
+            </div>
+
+            <div className="facultypop">Poster Link</div>
+            <div style={{ width: "-webkit-fill-available" }}>
+              <input
+                className="form-control input-field"
+                title={"Event Photo Link"}
+                onChange={(e) => setImg(e.target.value)}
+                placeholder={"Ex: Upload link like drive link"}
+                value={img}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div
+              style={{
+                marginTop: 20,
+                justifyContent: "center",
+                display: "flex",
+                marginRight: "20px",
+              }}
+            >
+              <Button  >
+              Submit
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 }
 
 export default PopF;
+
